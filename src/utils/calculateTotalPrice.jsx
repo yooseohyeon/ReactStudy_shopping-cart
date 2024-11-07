db1c6e98f89c2fef9ba27ac7cd1e0ab6649d3f03
yooseohyeon <yooseohyeon.02@gmail.com>
2024-11-07 14:08:48 +0900

    refactor: 상태 업데이트를 Context로 중앙화함에 따라, TotalPrice state를 계산하는 calculateTotalPrice 함수를 Context 내부로 이동함.

--- src/utils/calculateTotalPrice.jsx
+++
@@ -1,9 +0,0 @@
-// 체크박스가 선택된 상품들의 가격만 총 가격에 반영되도록 함
-export const calculateTotalPrice = (productList) => {
-  return productList.reduce((acc, product) => {
-    if (product.checked) {
-      return acc + product.price * product.quantity;
-    }
-    return acc;
-  }, 0);
-};
